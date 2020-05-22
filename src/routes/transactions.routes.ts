0334129a86911461395656cd87918b84c55f1f9a
Victor Hermes <me.victorhermes@gmail.com>
2020-05-21 22:09:59 -0300

    first commit

---
+++ src/routes/transactions.routes.ts
@@ -0,0 +1,46 @@
+import { getCustomRepository } from 'typeorm';
+import { Router } from 'express';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
+// import DeleteTransactionService from '../services/DeleteTransactionService';
+// import ImportTransactionsService from '../services/ImportTransactionsService';
+
+const transactionsRouter = Router();
+
+transactionsRouter.get('/', async (request, response) => {
+  const transactionsRepository = getCustomRepository(TransactionsRepository);
+  const transactions = await transactionsRepository.find();
+  const balance = await transactionsRepository.getBalance();
+
+  return response.json({ transactions, balance });
+});
+
+transactionsRouter.post('/', async (request, response) => {
+  try {
+    const { title, value, type, category } = request.body;
+
+    const createTransaction = new CreateTransactionService();
+
+    const transaction = await createTransaction.execute({
+      title,
+      value,
+      type,
+      category,
+    });
+
+    return response.json(transaction);
+  } catch (err) {
+    return response.json(err);
+  }
+});
+
+transactionsRouter.delete('/:id', async (request, response) => {
+  // TODO
+});
+
+transactionsRouter.post('/import', async (request, response) => {
+  // TODO
+});
+
+export default transactionsRouter;
